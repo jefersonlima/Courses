ba00e5c9d2217bb244bbfdf27ad41bd3478166a3
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-04-23 06:48:20 -0400

    segundo exercicio da lista

---
+++ Cursos/UdemyWebModernoCompletoComJS2021Projetos/exercicios-js/ExerciciosJavaScript/exercicio2.js
@@ -0,0 +1,24 @@
+/*
+02) Os triângulos podem ser classificados em 3 tipos quanto ao tamanho de seus lados:
+Equilátero: Os três lados são iguais. Isósceles: Dois lados iguais. Escaleno: Todos os lados são diferentes.
+Crie uma função que recebe os comprimentos dos três lados de um triângulo e retorne sua classificação quanto
+ao tamanho de seus lados. (Neste exemplo deve-se abstrair as condições matemáticas de existência de um
+triângulo).
+*/
+
+function gerarNumeroAleatorio(){
+    const valor = Math.random() * (4 - 1) + 1
+    return Math.floor(valor)
+}
+
+function verificaTipoTriangulo(lado1, lado2, lado3){
+    if (lado1 === lado2 && lado1 === lado3){
+        return 'Triangulo formado pelos lados: lado1 = ' + lado1 + ', lado2 = ' + lado2 + ', lado3 = ' + lado3 + ' é Equilátero'
+    }else if (lado1 === lado2 || lado1 === lado3 || lado2 === lado3){
+        return 'Triangulo formado pelos lados: lado1 = ' + lado1 + ', lado2 = ' + lado2 + ', lado3 = ' + lado3 + ' é Isósceles'
+    }else{
+        return 'Triangulo formado pelos lados: lado1 = ' + lado1 + ', lado2 = ' + lado2 + ', lado3 = ' + lado3 + ' é Escaleno'
+    }
+}
+
+console.log(verificaTipoTriangulo(gerarNumeroAleatorio(), gerarNumeroAleatorio(), gerarNumeroAleatorio()))
