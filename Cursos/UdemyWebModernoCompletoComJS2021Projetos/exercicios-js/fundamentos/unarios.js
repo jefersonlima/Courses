3b26b5c0a7fcb25fdc0036456b7224ccfca5a142
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-04-23 06:08:42 -0400

    add files

---
+++ Cursos/UdemyWebModernoCompletoComJS2021Projetos/exercicios-js/fundamentos/unarios.js
@@ -0,0 +1,9 @@
+let num1 = 1
+let num2 = 2
+
+num1++ // pór fixado
+console.log(num1)
+--num1 // pré fixado
+
+console.log(++num1 === num2--)
+console.log(num1 === num2)
---
+++ Cursos/UdemyWebModernoCompletoComJS2021Projetos/exercicios-js/fundamentos/usandoVar2.js
@@ -0,0 +1,7 @@
+// variavel VAR tem escopo Global e Escopo de função
+var numero = 1
+{
+    var numero = 2
+    console.log('dentro = ', numero)
+}
+console.log('fora = ', numero)
