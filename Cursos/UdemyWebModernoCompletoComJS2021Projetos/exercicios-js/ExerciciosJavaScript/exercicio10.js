6e1d7ef5a59667e13c1649f2e0a41ff3aef6f0d1
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-04-25 23:07:53 -0400

    exercicio 10 da lista

---
+++ Cursos/UdemyWebModernoCompletoComJS2021Projetos/exercicios-js/ExerciciosJavaScript/exercicio10.js
@@ -0,0 +1,18 @@
+/*
+10) Crie uma função que verifica se um número inteiro passado como parêmetro é divisível por 3 e retorne true
+ou false.
+*/
+
+function divisivelPorTres(numero){
+    if (numero % 3 === 0){
+        return true
+    }else{
+        return false
+    }
+}
+
+console.log(divisivelPorTres(3))
+console.log(divisivelPorTres(33))
+console.log(divisivelPorTres(333))
+console.log(divisivelPorTres(38))
+console.log(divisivelPorTres(337))
