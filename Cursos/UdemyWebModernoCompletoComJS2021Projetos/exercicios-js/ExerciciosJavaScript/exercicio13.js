004b95a9b6ba91ef154a2ba57750daef96a6c4cf
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-04-26 05:56:10 -0400

    exercicio 12 da lista

---
+++ Cursos/UdemyWebModernoCompletoComJS2021Projetos/exercicios-js/ExerciciosJavaScript/exercicio13.js
@@ -0,0 +1,24 @@
+/*
+13) Crie um programa que exibe se um dia é dia útil, fim de semana ou dia inválido dado o número referente ao
+dia. Considere que domingo é o dia 1 e sábado é o dia 7. Utilize a estrutura Switch.
+*/
+
+function geraNumeroAleatorio(){
+    const dia = Math.random() * (7 - 1) + 1
+    return Math.floor(dia)
+}
+
+function verificaDiaUtil(dia){
+    switch (dia){
+        case 1: case 7:
+            return 'Fim de Semana ' + dia
+            break
+        case 2: case 3: case 4: case 5: case 6:
+            return 'Dia util ' + dia
+            break
+        default:
+            return 'dia inválido ' + dia
+    }
+}
+
+console.log(verificaDiaUtil(geraNumeroAleatorio()))
