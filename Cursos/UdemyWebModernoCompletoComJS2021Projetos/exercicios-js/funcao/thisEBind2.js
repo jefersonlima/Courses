16c4c0496bd6c95b3827fcc35712c6ee7b7e65ac
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-04-27 05:28:28 -0400

    outro exemplo this and bind

---
+++ Cursos/UdemyWebModernoCompletoComJS2021Projetos/exercicios-js/funcao/thisEBind2.js
@@ -0,0 +1,29 @@
+function Pessoa(){
+    this.idade = 0
+
+    //errado
+    /*
+    setInterval(function(){
+        this.idade++
+        console.log(this.idade)
+    }, 1000)
+    */
+
+    //certo
+    setInterval(function(){
+        this.idade++
+        console.log(this.idade)
+    }.bind(this), 1000)
+
+    //muito encontrado mas errado(funciona)
+    /*
+    const self = this
+    setInterval(function(){
+        self.idade++
+        console.log(self.idade)
+    }, 1000)
+    */
+
+}
+
+new Pessoa
