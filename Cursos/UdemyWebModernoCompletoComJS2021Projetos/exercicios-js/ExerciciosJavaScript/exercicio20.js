425eeeaff8a8375954f04aec8b16d3fd9dc9d761
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-04-28 06:42:06 -0400

    exercicio 21 da lista

--- Cursos/UdemyWebModernoCompletoComJS2021Projetos/exercicios-js/ExerciciosJavaScript/exercicio20.js
+++ Cursos/UdemyWebModernoCompletoComJS2021Projetos/exercicios-js/ExerciciosJavaScript/exercicio20.js
@@ -7,31 +7,31 @@ nota(s) de R$ 10. 1 nota(s) de R$ 5. 3 nota(s) de R$ 1.
 */
 
 function notasTroco(valor){
-    let recebido = valor
     let nota = 0
-    if (recebido >= 100){
-        nota = Math.floor(recebido / 100)
-        recebido = recebido % 100
+    console.log('Troco')
+    if (valor >= 100){
+        nota = Math.floor(valor / 100)
+        valor = valor % 100
         console.log(nota + ' nota(s) de R$ 100')
     }
-    if (recebido >= 50){
-        nota = Math.floor(recebido / 50)
-        recebido = recebido % 50
+    if (valor >= 50){
+        nota = Math.floor(valor / 50)
+        valor = valor % 50
         console.log(nota + ' nota(s) de R$ 50')
     }
-    if (recebido >= 00){
-        nota = Math.floor(recebido / 10)
-        recebido = recebido % 10
+    if (valor >= 00){
+        nota = Math.floor(valor / 10)
+        valor = valor % 10
         console.log(nota + ' nota(s) de R$ 10')
     }
-    if (recebido >= 5){
-        nota = Math.floor(recebido / 5)
-        recebido = recebido % 5
+    if (valor >= 5){
+        nota = Math.floor(valor / 5)
+        valor = valor % 5
         console.log(nota + ' nota(s) de R$ 5')
     }
-    if (recebido >= 1){
-        nota = Math.floor(recebido / 1)
-        recebido = recebido % 1
+    if (valor >= 1){
+        nota = Math.floor(valor / 1)
+        valor = valor % 1
         console.log(nota + ' nota(s) de R$ 1')
     }
     console.log()
