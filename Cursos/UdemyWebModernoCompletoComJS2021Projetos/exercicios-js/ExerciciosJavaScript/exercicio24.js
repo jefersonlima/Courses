f149020ad0074c6439ba1c07b941042c57bc466b
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-04-29 07:54:48 -0400

    exercicio 24 da lista

---
+++ Cursos/UdemyWebModernoCompletoComJS2021Projetos/exercicios-js/ExerciciosJavaScript/exercicio24.js
@@ -0,0 +1,14 @@
+/*
+24) Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.
+*/
+
+function imprimirString(quantidade = 11, frase = 'Hello World!') {
+    let contador = 0
+    while(contador < quantidade){
+        console.log(frase)
+        contador ++
+    }
+}
+
+imprimirString()
+imprimirString(5, 'Teste')
