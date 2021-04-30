9e234fdc4358f198b70147c21fe8d87866b4f4c9
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-04-30 06:10:31 -0400

    Projeto Leitura

---
+++ Cursos/UdemyWebModernoCompletoComJS2021Projetos/exercicios-js/projetos/Leitura.js
@@ -0,0 +1,20 @@
+let textoDividido
+let posicao = 0
+function leituraStatica(texto){
+    textoDividido = texto.split(' ')    
+    imprimir()
+}
+function imprimir(){   
+        setInterval(function(){
+            if(posicao < textoDividido.length){
+                console.log(textoDividido[posicao])
+                posicao++
+            }else{
+                //decobrir como parar o setInterval
+                clearInterval(null)
+            }
+        }.bind(this), 1000)
+}
+//a ideia é pegar um arquivo de texto ou pdf como entrada
+let textoTeste = 'Se o separator contiver vários caracteres, toda a sequência de caracteres deve ser encontrada para ser dividida.'
+leituraStatica(textoTeste)
