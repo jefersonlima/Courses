d80e940fb38249d57015679fe32a900697dc78ff
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-05-01 05:46:46 -0400

    Orientação a objeto

---
+++ Cursos/UdemyWebModernoCompletoComJS2021Projetos/exercicios-js/objeto/oo.js
@@ -0,0 +1,21 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento() // o foco  passou a ser o objeto
+
+// Principios importantes:
+// 1. abstracao
+// 2. encapsulamento
+// 3. herança (prototype)
+// 4. polimorfismo
