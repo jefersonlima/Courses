001280f8a1c8767bdf206ec6aad6a5e068ab0c51
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-05-03 13:56:59 -0400

    Map

---
+++ Cursos/UdemyWebModernoCompletoComJS2021Projetos/exercicios-js/secao7Array/map2.js
@@ -0,0 +1,22 @@
+const carrinho = [
+    '{ "nome": "Borracha", "preco": 3.45 }',
+    '{ "nome": "Caderno", "preco": 13.90 }',
+    '{ "nome": "Kit de Lapis", "preco": 41.22 }',
+    '{ "nome": "Caneta", "preco": 7.50 }'
+]
+
+//retornar um array apenas com os preços
+
+//const array = JSON.parse(carrinho)
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(resultado)
+
+const resultado2 = carrinho.map(paraObjeto)
+console.log(resultado2)
+
+
+
