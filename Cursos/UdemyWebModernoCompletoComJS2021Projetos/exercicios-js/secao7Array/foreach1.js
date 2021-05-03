7abd1308c710ec741a7ff2e87b7d2a6c861523bb
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-05-03 06:05:32 -0400

    ForEach

---
+++ Cursos/UdemyWebModernoCompletoComJS2021Projetos/exercicios-js/secao7Array/foreach1.js
@@ -0,0 +1,13 @@
+const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
+
+//aprovados.forEach(function(nome, indice, array, x) {
+aprovados.forEach(function(nome, indice) {
+    console.log(`${indice + 1}) ${nome}`)
+    console.log(array)
+    console.log(x)
+})
+
+aprovados.forEach(nome => console.log(nome))
+
+const exibirAprovados = aprovado => console.log(aprovado)
+aprovados.forEach(exibirAprovados)
