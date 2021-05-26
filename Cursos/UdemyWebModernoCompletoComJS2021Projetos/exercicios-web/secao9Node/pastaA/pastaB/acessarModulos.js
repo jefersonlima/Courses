5198fe07165af89e6f5fdf33a11d1f62b4f89815
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-05-25 22:33:48 -0400

    Lodash

---
+++ Cursos/UdemyWebModernoCompletoComJS2021Projetos/exercicios-web/secao9Node/pastaA/pastaB/acessarModulos.js
@@ -0,0 +1,15 @@
+const moduloA = require('../../moduloA')
+//const moduloA = require('C:/Cursos/Cursos/UdemyWebModernoCompletoComJS2021Projetos/exercicios-web/secao9Node/moduloA.js')
+console.log(moduloA.ola)
+
+const saudacao = require('saudacao')
+console.log(saudacao.ola)
+
+const c = require('./pastaC')
+console.log(c.ola2)
+
+const http = require('http')
+http.createServer((req, res) => {
+    res.write('Bom dia!')
+    res.end()
+}).listen(8080)
