351e1dfc08a8fab58297c668afbce8327866c064
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-05-26 07:09:11 -0400

    resolvendo desafio

--- Cursos/UdemyWebModernoCompletoComJS2021Projetos/exercicios-web/secao9Node/funcionarios/funcionarios.js
+++ Cursos/UdemyWebModernoCompletoComJS2021Projetos/exercicios-web/secao9Node/funcionarios/funcionarios.js
@@ -1,7 +1,26 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
+const chineses = funcionario => funcionario.pais === 'China'
+const mulheres = funcionario => funcionario.genero === 'F'
+const menorSalario = (func, funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual
+}
+
 axios.get(url).then(response =>{
     const funcionarios = response.data
     console.log(funcionarios)
+ 
+//console.log(funcionarios.filter(chineses).filter(mulheres).filter(menorSalario))
+const func = funcionarios
+    .filter(chineses)
+    .filter(mulheres)
+    .reduce(menorSalario)
+
+console.log(func)
+
 })
+
+
+
+
